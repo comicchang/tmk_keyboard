175e15d2fd05a1e891c85a5e97864f2121ca9199
Comic Chang <comicchang@gmail.com>
2017-03-17 14:16:57 +0800

    optimze for windoge

--- converter/usb_usb/keymap.c
+++ converter/usb_usb/keymap.c
@@ -19,6 +19,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#include "eeconfig.h"
 #define ____ TRNS
 
+enum keymap_layout {
+    SAFE = 0,
+    SPCFN,
+    MAC,
+    SPACEFN,
+    MOUSE,
+    PASSWD,
+};
+
+
+
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     /* layer 0: Base layer
      * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
@@ -36,11 +47,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |Ctl|Gui|Alt|           Space               |Alt|Gui|App|Ctl| |Lef|Dow|Rig| |      0|  .|Ent|
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
-    KEYMAP(
+    [SAFE] = KEYMAP(
     ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,      PSCR,SLCK,BRK,
     GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL, END, PGDN,    P7,  P8,  P9,
-    LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         P4,  P5,  P6,  PPLS,
+    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         P4,  P5,  P6,  PPLS,
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,          UP,           P1,  P2,  P3,
     LCTL,LGUI,LALT,          SPC,                     RALT,RGUI, APP,FN1,      LEFT,DOWN,RGHT,    P0,       PDOT,PENT
     ),
@@ -61,13 +72,38 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |Ctl|Gui|Alt|           Space               |Alt|Gui|App|Ctl| |Lef|Dow|Rig| |      0|  .|Ent|
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
-    KEYMAP(
+    [SPCFN] = KEYMAP(
+    ____,     ____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,
+    ____,____,____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,    ____,____,____,____,
+    ____,____,____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,    ____,____,____,
+    FN6 ,____,____,____,____,____,____,____,____,____,____,____,     ____,                       ____,____,____,____,
+    ____,____,____,____,____,____,____,____,____,____,____,          ____,         ____,         ____,____,____,
+    ____,____,____,          FN0,                     ____,____,____,____,    ____,____,____,    ____,     ____,____
+    ),
+
+    /* MAC layout
+    * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
+     * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau|
+     * `---'   `---------------' `---------------' `---------------' `-----------'
+     * ,-----------------------------------------------------------. ,-----------. ,---------------.
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|    Bsp| |Ins|Hom|PgU| |NmL|  /|  *|  -|
+     * |-----------------------------------------------------------| |-----------| |---------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  | |Del|End|PgD| |  7|  8|  9|  +|
+     * |-----------------------------------------------------------| `-----------' |-----------|   |
+     * |CapsL |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Return|               |  4|  5|  6|   |
+     * |-----------------------------------------------------------|     ,---.     |---------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shift     |     |Up |     |  1|  2|  3|   |
+     * |-----------------------------------------------------------| ,-----------. |-----------|   |
+     * |Ctl|Gui|Alt|           Space               |Alt|Gui|App|Ctl| |Lef|Dow|Rig| |      0|  .|Ent|
+     * `-----------------------------------------------------------' `-----------' `---------------'
+     */
+    [MAC] = KEYMAP(
     ____,     ____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,
     ____,____,____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,    ____,____,____,____,
     ____,____,____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,    ____,____,____,
     FN5 ,____,____,____,____,____,____,____,____,____,____,____,     ____,                       ____,____,____,____,
     ____,____,____,____,____,____,____,____,____,____,____,          ____,         ____,         ____,____,____,
-    ____,____,____,          FN0,                     ____,____,____,____,    ____,____,____,    ____,     ____,____
+    ____,LALT,LGUI,         ____,                     RGUI,RALT,____,____,    ____,____,____,    ____,     ____,____
     ),
 
     /* Overlay 2: SpaceFN
@@ -87,7 +123,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
 
-    KEYMAP(
+    [SPACEFN] = KEYMAP(
     ____,     ____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,
     ____,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,    ____,____,____,    ____,____,____,____,
     ____,____,PGUP,____,____,____,____,____,____,PSCR,SLCK,PAUS, INS,____,    ____,____,____,    ____,____,____,
@@ -113,7 +149,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
 
-    KEYMAP(
+    [MOUSE] = KEYMAP(
     FN30,     F14 ,F15 ,FN3 ,FN4 ,____,____,MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,    ____,____,____,
     FN30,F14 ,F15 ,FN3 ,FN4 ,____,____,MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,____,    ____,____,____,    ____,____,____,____,
     ____,BTN2,MS_U,BTN1,WH_U,____,____,____,UP  ,____,____,____,____,____,    ____,____,____,    ____,____,____,
@@ -139,10 +175,10 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
 
-    KEYMAP(
-    FN21,     FN22,FN23,FN24,FN25,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,    FN21,FN21,FN21,
+    [PASSWD] = KEYMAP(
+    FN21,     FN30,FN31,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,    FN21,FN21,FN21,
     FN21,FN22,FN23,FN24,FN25,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,    FN21,FN21,FN21,    FN21,FN21,FN21,FN21,
-    FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,    FN21,FN21,FN21,    FN21,FN21,FN21,
+    FN21,FN21,FN21,FN21,FN29,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,    FN21,FN21,FN21,    FN21,FN21,FN21,
     FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,____,     FN21,                       FN21,FN21,FN21,FN21,
     FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,FN21,          FN21,         FN21,         FN21,FN21,FN21,
     FN21,FN21,FN21,         FN21,                     FN21,FN21,FN21,____,    FN21,FN21,FN21,    FN21,     FN21,FN21
@@ -166,7 +202,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
     /*
-    KEYMAP(
+    [LAYER] = KEYMAP(
     ____,     ____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,
     ____,____,____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,    ____,____,____,____,
     ____,____,____,____,____,____,____,____,____,____,____,____,____,____,    ____,____,____,    ____,____,____,
@@ -182,7 +218,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 enum function_id {
     TRICKY_ESC,
     DOUBLE_CTRL,
-    SET_DEFAULT_LAYER,
+    SET_DEFAULT_LAYER_SPCFN,
+    SET_DEFAULT_LAYER_MAC,
+    JUMP_BOOTLOADER,
 };
 enum macro_id {
     CLEAR,
@@ -194,33 +232,36 @@ enum macro_id {
 /*
  * Fn action definition
  */
-const action_t fn_actions[] PROGMEM  = {
-    [0] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),                                // SPACE LAYER
-    [1] = ACTION_LAYER_MOMENTARY(3),                                        // MOUSE LAYER
-    [2] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),                                // tilde
+
+const action_t fn_actions[] PROGMEM = {
+
+    [0]  = ACTION_LAYER_TAP_KEY(SPACEFN, KC_SPACE),                          // SPACE LAYER
+    [1]  = ACTION_LAYER_MOMENTARY(MOUSE),                                    // MOUSE LAYER
+    [2]  = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),                                // tilde
 
     // Media keys in mousekey layer
-    [3] = ACTION_MODS_KEY(MOD_LCTL, KC_UP),                                 // Mission Control
-    [4] = ACTION_MODS_KEY(MOD_LALT|MOD_LGUI, KC_L),                         // LaunchCenter
-
-    [5] = ACTION_MODS_TAP_KEY(MOD_LCTL|MOD_LSFT|MOD_LALT|MOD_LGUI, KC_F18), // Soft MAGIC KEY
-    //[6] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),                            // click LCTL without other key = ESC
-    //[7] = ACTION_FUNCTION_TAP(DOUBLE_CTRL),
+    [3]  = ACTION_MODS_KEY(MOD_LCTL, KC_UP),                                 // Mission Control
+    [4]  = ACTION_MODS_KEY(MOD_LALT|MOD_LGUI, KC_L),                         // LaunchCenter
+    [5]  = ACTION_MODS_TAP_KEY(MOD_LCTL|MOD_LSFT|MOD_LALT|MOD_LGUI, KC_F18), // Soft MAGIC KEY
+    [6]  = ACTION_MODS_TAP_KEY(MOD_LCTL|MOD_LSFT|MOD_LALT|MOD_LGUI, KC_CAPS),// Soft MAGIC KEY
 
     // Shift + ESC = ~
-    [8] = ACTION_FUNCTION (TRICKY_ESC),
+    [8]  = ACTION_FUNCTION (TRICKY_ESC),
 
     // FN + ' = password layer
-    [20] = ACTION_LAYER_INVERT(4,ON_BOTH),
-    [21] = ACTION_LAYER_OFF(4,ON_PRESS),
+    [20] = ACTION_LAYER_INVERT(PASSWD,ON_BOTH),
+    [21] = ACTION_LAYER_OFF(PASSWD,ON_PRESS),
     [22] = ACTION_MACRO(CLEAR),
     [23] = ACTION_MACRO(PASSWORD1),
     [24] = ACTION_MACRO(PASSWORD2),
     [25] = ACTION_MACRO(PASSWORD3),
 
-    // FN + ESC = Toggle SpaceFN off (or game mode)
-    [30] = ACTION_LAYER_INVERT(1, ON_PRESS),
-    [31] = ACTION_FUNCTION (SET_DEFAULT_LAYER),
+    // FN + ' + F1~F2 = Toggle layers
+    // FN + ' + r = reboot to bootloader
+    [29] = ACTION_FUNCTION (JUMP_BOOTLOADER),
+    [30] = ACTION_FUNCTION (SET_DEFAULT_LAYER_SPCFN),
+    [31] = ACTION_FUNCTION (SET_DEFAULT_LAYER_MAC),
+
 };
 
 
@@ -240,30 +281,44 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 }
 
 #define MODS_SHIFT_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+extern uint32_t default_layer_state ;
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     static uint8_t tricky_esc_registered;
     switch (id) {
-        case SET_DEFAULT_LAYER:
-            //eeconfig_write_default_layer((1<<1));
-            break;
-//        case DOUBLE_CTRL:
-        case TRICKY_ESC:
-            if (record->event.pressed) {
-                if (get_mods() & MODS_SHIFT_MASK) {
-                    tricky_esc_registered = KC_GRV;
-                }
-                else {
-                    tricky_esc_registered = KC_ESC;
-                }
-                add_key(tricky_esc_registered);
-                send_keyboard_report();
-            }
-            else {
-                del_key(tricky_esc_registered);
-                send_keyboard_report();
-            }
-        default:
-            break;
+	case SET_DEFAULT_LAYER_MAC:
+	    if (!record->event.pressed) {
+		default_layer_xor((1UL<<MAC));
+		eeconfig_write_default_layer((uint8_t)(default_layer_state));
+	    }
+	    break;
+	case SET_DEFAULT_LAYER_SPCFN:
+	    if (!record->event.pressed) {
+		default_layer_xor((1UL<<SPCFN));
+		eeconfig_write_default_layer((uint8_t)(default_layer_state));
+	    }
+	    break;
+	case JUMP_BOOTLOADER:
+	    if (!record->event.pressed) {
+		bootloader_jump();
+	    }
+	    break;
+	case TRICKY_ESC:
+	    if (record->event.pressed) {
+		if (get_mods() & MODS_SHIFT_MASK) {
+		    tricky_esc_registered = KC_GRV;
+		}
+		else {
+		    tricky_esc_registered = KC_ESC;
+		}
+		add_key(tricky_esc_registered);
+		send_keyboard_report();
+	    }
+	    else {
+		del_key(tricky_esc_registered);
+		send_keyboard_report();
+	    }
+	default:
+	    break;
     }
 }
